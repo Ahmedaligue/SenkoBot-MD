dc8f70dea47c1af4d7d5dc9d95926ebd56685b08
Ell Contrerax <sarmientodieg@gmail.com>
2025-10-02 13:52:33 -0500

    Actualizar _antilinks.js

--- plugins/_antilinks.js
+++ plugins/_antilinks.js
@@ -1,7 +1,7 @@
 const linkRegex = /(chat\.whatsapp\.com\/[0-9A-Za-z]{20,24})|(z?https:\/\/whatsapp\.com\/channel\/[0-9A-Za-z]{20,24})/i
 const allowedLinks = ['https://whatsapp.com/channel/0029VbApwZ9ISTkEBb6ttS3F']
 
-export async function before(m, { conn, isAdmin, isBotAdmin, isModeration }) {
+export async function before(m, { conn, isAdmin, isBotAdmin, isOwner }) {
 if (!m.isGroup) return
 
 const chat = globalThis?.db?.data?.chats[m.chat]
@@ -11,7 +11,7 @@ const isGroupLink = linkRegex.test(m.text)
 const hasAllowedLink = allowedLinks.some(link => m.text.includes(link))
 if (hasAllowedLink) return
 
-if (chat.antilinks && isGroupLink && !isAdmin && isBotAdmin && !isModeration && m.key.participant !== conn.user.jid) {
+if (chat.antilinks && isGroupLink && !isAdmin && isBotAdmin && !isOwner && m.key.participant !== conn.user.jid) {
 await conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: m.key.id, participant: m.key.participant }})
 await conn.reply(m.chat, `*${globalThis.db.data.users[m.key.participant].name}* eliminado por \`Anti-Link\``, m)
 await conn.groupParticipantsUpdate(m.chat, [m.key.participant], 'remove')
